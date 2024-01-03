a5b5179209b65cbcb31f6252c1c5725d430d7333
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-03 21:20:37 +0530

    Create LaserBeams.java

---
+++ medium/LaserBeams.java
@@ -0,0 +1,23 @@
+//2125. Number of Laser Beams in a Bank
+//https://leetcode.com/problems/number-of-laser-beams-in-a-bank/description/?envType=daily-question&envId=2024-01-03
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int prev = 0, ans = 0;
+
+    for (String s: bank) {
+      int count = 0;
+      for (int i = 0; i < s.length(); i++)
+        if (s.charAt(i) == '1') {
+          count++;
+        }
+
+      if (count > 0) {
+        ans += prev * count;
+        prev = count;
+      }
+    }
+
+    return ans;
+    }
+}
+//O(M * N)
