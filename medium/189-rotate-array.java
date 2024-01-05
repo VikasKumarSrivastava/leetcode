3d8aaa06489e1b2c442358bbb57ef5f03800394e
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-01-05 23:39:26 +0530

    Create 189-rotate-array.java

---
+++ medium/189-rotate-array.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/rotate-array/description/
+class Solution {
+    public static void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        int size = nums.length;      
+        k =k%size;
+        reverse(nums,0,size-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,size-1);
+    }
+}
