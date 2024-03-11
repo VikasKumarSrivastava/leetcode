2c807610933763137442c8ddcc85e9aacc7cf9c8
VikasKumarSrivastava <42264065+VikasKumarSrivastava@users.noreply.github.com>
2024-03-11 22:27:54 +0530

    Update Peak_Element.java

--- medium/Peak_Element.java
+++ medium/Peak_Element.java
@@ -2,7 +2,6 @@ class Solution {
     public int findPeakElement(int[] nums) {
         int lo =0;
         int hi =nums.length-1;
-        
         while(lo<hi){
             int mid =lo + (hi-lo)/2;
             if(nums[mid] < nums[mid+1]){
